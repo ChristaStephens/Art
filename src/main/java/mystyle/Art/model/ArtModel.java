0f5ca47818b5a650f9e9504787c129a878676681
Christa Stephens <ChristaStephens@outlook.com>
2019-01-08 13:32:58 -0500

    switched model to entity to create a table and dao

--- src/main/java/mystyle/Art/model/ArtModel.java
+++ src/main/java/mystyle/Art/model/ArtModel.java
@@ -1,7 +1,21 @@
 package mystyle.Art.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "art_style")
 public class ArtModel {
 	
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="user_id")
 	protected Long id;
 	protected String name;
 	protected String last;
